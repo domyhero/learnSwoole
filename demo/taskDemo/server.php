db3a1abd7c0c98f8293d67e8761964143f9c43c3
keepondream <402314889@qq.com>
2018-05-07 23:18:55 +0800

    说明: taskWorker中的全局变量不能适用于各个进程之间的内存共享,只能独立且初始时只能得到全局变量的初始值

--- demo/taskDemo/server.php
+++ demo/taskDemo/server.php
@@ -1,4 +1,6 @@
 <?php
+
+//----------此方法里面使用的全局变量,只针对各个进程,并不能做到各个进程之间内存共享---------
 /**
  * Created by PhpStorm.
  * User: dream
@@ -10,6 +12,7 @@ use learnswoole\common\tcpPack;
 use learnswoole\common\common;
 //定义一个全局task工作进程状态数组
 $task_worker_ids = [];
+$test = '苹果';
 //实例化服务端
 $serv = new swoole_server('0.0.0.0',9501,SWOOLE_PROCESS,SWOOLE_SOCK_TCP);
 //配置
@@ -27,33 +30,45 @@ $serv->set([
 ]);
 //主进程启动
 $serv->on('start', function ($serv) {
+    common::dump('主进程启动');
+    common::dump($GLOBALS['test']);
+    $GLOBALS['test'] = '香蕉';
 
 });
 //管理进程启动
 $serv->on('managerStart', function ($serv) {
-
+        common::dump('管理进程启动');
+        common::dump($GLOBALS['test']);
 });
 //工作进程启动
 $serv->on('workerStart', function ($serv, $worker_id) {
+    common::dump('工作进程启动');
+    common::dump($GLOBALS['test']);
     //因为每个进程之间是独立存在的,所以给每个工作进程加载文件,这里有worker进程和taskworker进程
     require_once dirname(dirname(__DIR__)).DIRECTORY_SEPARATOR.'autoloader.php';
     //设置全局task变量的初始状态
-    if ($worker_id == 0) {
+//    if ($worker_id == 0) {
         $task_worker_num = $serv->setting['task_worker_num'];
         $tempArr = [];
         for ($i = 0 ;$i < $task_worker_num; $i++) {
             $tempArr[]  = $i;
         }
         $GLOBALS['task_worker_ids'] = $tempArr;
-    }
+//    }
 
 });
 //监听客户端连接触发
 $serv->on('connect', function ($serv, $fd) {
+    common::dump('客户端连接');
+    common::dump($GLOBALS['test']);
+    $GLOBALS['test'] = '客户端改苹果';
+    common::dump($GLOBALS['test']);
     common::dump("有新的连接 标识 : {$fd} ");
 });
 //监听接收客户端消息触发
 $serv->on('receive', function ($serv, $fd, $reactor_id, $data) {
+    common::dump('接收到客户端消息');
+    common::dump($GLOBALS['test']);
     //处理客户端消息
     $body = tcpPack::de_pack_body($data);
     if (!empty($body) && $body['type'] == 'task') {
@@ -88,6 +103,8 @@ $serv->on('close', function ($serv, $fd, $reactor_id) {
 });
 //监听有worker进程,调用的taskWorker时触发
 $serv->on('task', function ($serv, $task_id, $src_worker_id, $data) {
+    common::dump('taskWorke');
+    common::dump($GLOBALS['test']);
     common::dump("task工作开始进程 任务ID : {$task_id}, 来自工作进程 {$src_worker_id}");
     $outData = ['success'=>['src_task_id'=>$data['src_task_id']]];
     sleep(5);
