75c7e72961b889050e3f406fe7025dadf41103d9
keepondream <402314889@qq.com>
2018-05-08 01:06:52 +0800

    封装服务端,并测试task任务排队数量

--- common/common.php
+++ common/common.php
@@ -20,7 +20,7 @@ class common
     {
 //        echo PHP_EOL."输出内容如下: ".PHP_EOL;
         if (is_string($data)) {
-            echo $data.PHP_EOL;
+            echo $data . PHP_EOL;
         } else {
             var_dump($data);
             echo PHP_EOL;
@@ -44,6 +44,60 @@ class common
             }
         }
         return false;
+    }
+
+    //服务端swoole配置
+    public static function server_tcp_configs()
+    {
+        $configs = [
+            'worker_num' => 3,              //worker进程数量
+            'reactor_num' => 4,             //reactor线程数量
+            'task_worker_num' => 2,         //task_worker进程数量
+            'max_request' => 1000,          //worker进程最大任务数,用于解决PHP进程内存溢出问题
+            //'daemonize' => true,            //开启守护进程
+            //'log_file' =>__DIR__ . 'server.log',    //记录server日志
+            //'heartbeat_check_interval' =>60,    //启动心跳检测 60秒检测一次
+            //'heartbeat_idle_time' => 600,       //最大允许连接空闲时间 超过则强制关闭该连接
+            'open_length_check' => true,    //打开包长检测
+            'package_max_length' => 1024 * 1024 * 2,    //最大数据包尺寸 2兆 此属性设置越大占用内存越大
+            'package_length_type' => 'N',   //长度值类型:   N 无符号、网络字节序、4字节
+            'package_body_offset' => 4,     //从第几个字节开始计算长度 包体
+            'package_length_offset' => 0,   //length长度值在包头的第几个字节。
+        ];
+        return $configs;
+    }
+
+    //客户端配置
+    public static function client_tcp_configs()
+    {
+        $configs = [
+            'open_length_check' => true,    //打开包长检测
+            'package_max_length' => 1024 * 1024 * 2,    //最大数据包尺寸 2兆 此属性设置越大占用内存越大
+            'package_length_type' => 'N',   //长度值类型:   N 无符号、网络字节序、4字节
+            'package_body_offset' => 4,     //从第几个字节开始计算长度 包体
+            'package_length_offset' => 0,   //length长度值在包头的第几个字节。
+        ];
+        return $configs;
+    }
+
+    //客户端/服务端 连接配置
+    public static function connect_config($name=null)
+    {
+        $connectConfig = [
+            'servHost' => '0.0.0.0',
+            'servPort' => 9501,
+            'servMode' => SWOOLE_PROCESS,
+            'servSockType' => SWOOLE_SOCK_TCP,
+            'cliHost' => '127.0.0.1',
+            'cliPort' => 9501,
+            'cliSockType' => SWOOLE_SOCK_TCP,
+            'cliAsync' => SWOOLE_SOCK_ASYNC,
+        ];
+        if (!empty($name)) {
+            isset($connectConfig[$name]) || exit('ERROR : you can input servHost|servPort|servMode|servSockType|cliHost|cliPort|cliSockType|cliAsync  String');
+            return $connectConfig[$name];
+        }
+        return $connectConfig;
     }
 
 }
---
+++ demo/tableDemo/jobServer.php
@@ -0,0 +1,104 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: dream
+ * Date: 2018/5/7
+ * Time: 下午11:25
+ */
+
+require_once dirname(dirname(__DIR__)).DIRECTORY_SEPARATOR.'autoloader.php';
+
+use learnswoole\common\common;
+use learnswoole\common\tcpPack;
+
+class jobServer
+{
+    private $serv;
+
+    private $config;
+
+    public function __construct($config= [],$connectConfig=[])
+    {
+        (!empty($config) && !empty($connectConfig)) || exit("invalid configs , ERROR: ".__FILE__." line: ".__LINE__);
+        //全局对象
+        $this->serv = new \swoole\server($connectConfig['servHost'],$connectConfig['servPort'],$connectConfig['servMode'],$connectConfig['servSockType']);
+
+        //配置
+        $this->serv->set($config);
+
+
+        //注册事件
+        $this->serv->on('start',[$this,'onStart']);
+        $this->serv->on('managerStart',[$this,'onManagerStart']);
+        $this->serv->on('workerStart',[$this,'onWorkerStart']);
+        $this->serv->on('connect',[$this,'onConnect']);
+        $this->serv->on('receive',[$this,'onReceive']);
+        $this->serv->on('close',[$this,'onClose']);
+        $this->serv->on('task',[$this,'onTask']);
+        $this->serv->on('finish',[$this,'onFinish']);
+
+        //启动服务
+        $this->serv->start();
+    }
+
+    //主进程启动
+    public function onStart($serv)
+    {
+
+    }
+
+    //管理进程启动
+    public function onManagerStart($serv)
+    {
+
+    }
+
+    //工作进程启动
+    public function onWorkerStart($serv,$worker_id)
+    {
+
+    }
+
+    //连接客户端触发
+    public function onConnect($serv,$fd,$reactor_id)
+    {
+        //有客户端连接时 投递任务到task进程当中
+//        for ($i = 0 ; $i < 18; $i++) {
+        $i = 11;
+            $serv->task($i);
+            common::dump('投递 任务 :'.$i);
+            sleep(5);//阻塞3秒
+//        }
+
+    }
+
+    //接收客户端消息触发
+    public function onReceive($serv,$fd,$reactor_id,$data)
+    {
+
+    }
+
+    //客户端连接关闭触发
+    public function onClose($serv,$fd,$reactor_id)
+    {
+
+    }
+
+    //接收到工作进程投递任务task_worker事件触发
+    public function onTask($serv,$task_id,$src_worker_id,$data)
+    {
+        common::dump('当前排队的task任务数量 : '.$serv->stats()['tasking_num']);
+        common::dump('消息任务编号为: '.$task_id.',来自worker: '.$src_worker_id.' 的投递');
+        sleep(10);//阻塞10秒;
+        return '完毕了';
+    }
+
+    //task_worker 任务执行完毕触发
+    public function onFinish($serv,$task_id,$data)
+    {
+        common::dump('消息任务编号为: '.$task_id.' '.$data);
+
+    }
+}
+
+new jobServer(common::server_tcp_configs(),common::connect_config());
